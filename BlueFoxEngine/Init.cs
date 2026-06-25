7767ac67bead73af9054db6f14c01b983f876137
barrer898 <allrightpine@gmail.com>
2026-06-25 15:33:09 +0200

    Adding files i have forgoten to add :P

---
+++ BlueFoxEngine/Init.cs
@@ -0,0 +1,31 @@
+﻿using BlueFoxEngine.Logging;
+using BlueFoxEngine.Configuration;
+
+namespace BlueFoxEngine;
+
+static class Init
+{
+    private static Logger _logger = new Logger("EngineInit");
+    static void Main(string[] args) // Bootstrap/Entery point YAY
+    {
+        _logger.Output(Logger.OutputType.Notice, $"\n=====================\n" +
+                                                 $"BlueFoxEngine {EngineInfo.EngineVersionString}\n" +
+                                                 $"Built: {EngineInfo.EngineBuildDate}\n" +
+                                                 $"=====================", Logger.OutputLevel.Info);
+        _logger.Output(Logger.OutputType.Info, "Reading EngineConfig...", Logger.OutputLevel.Debug);
+        try
+        {
+            string engineConfigPath = Path.Combine(AppContext.BaseDirectory, "Configuration", "EngineConfig.json");
+            CurrentEngineConfig._EngineConfig = ConfigReader.LoadFirst(engineConfigPath);
+        }
+        catch (Exception e)
+        {
+            _logger.Output(Logger.OutputType.CriticalError, "Failed to load EngineConfig.", e, Logger.OutputLevel.Critical);
+            Environment.Exit(0);
+        }
+        _logger.Output(Logger.OutputType.Info, "Loaded EngineConfig, Updating Logger...", Logger.OutputLevel.Debug);
+        _logger.UpdateOutputLevel();
+        _logger.Output(Logger.OutputType.Info, "Done.", Logger.OutputLevel.Debug);
+        _logger.Output(Logger.OutputType.Info, "Loading Raylib...", Logger.OutputLevel.Info);
+    }
+}
